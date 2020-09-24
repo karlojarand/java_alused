b0bb74dc0cbf13107572969f704b8593679ced47
Karl Ojarand <karl@play.ee>
2020-09-24 20:14:13 +0300

    Esimesed ülesanded

---
+++ src/Main.java
@@ -0,0 +1,92 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //ÜLESANNE 1
+
+        /*String name = "Karl Ojarand";
+        System.out.println(name);
+
+            //ÜLESANNE 5
+        int secondsInHour = 60 * 60;
+        int secondsInDay = secondsInHour * 24;
+        int secondsInYear = secondsInDay * 365;
+        System.out.println("There are " + secondsInYear + " seconds in a year");
+
+        //ÜLESANNE 9
+        Scanner input = new Scanner(System.in);
+        System.out.print("Type a number: ");
+        int numberOne = Integer.parseInt(input.nextLine());
+        System.out.print("Type another number ");
+        int numberTwo = Integer.parseInt(input.nextLine());
+        double division = (double)numberOne / numberTwo;
+        System.out.println("Division: " + numberOne + "/" + numberTwo + "= " + division);
+
+
+*/          //ÜLESANNE 10
+/*        Scanner input = new Scanner(System.in);
+        System.out.print("Type the radius: ");
+        int radius = Integer.parseInt(input.nextLine());
+        double circumference = 2 * Math.PI * radius;
+        System.out.println("Circumference of the circle: " + circumference);*/
+
+        //ÜLESANNE 14
+/*        Scanner input = new Scanner(System.in);
+        System.out.print("Type a number: ");
+        int numberWritten = Integer.parseInt(input.nextLine());
+        if (numberWritten >= 0) {
+            System.out.println("The number is positive");
+        } else {
+            System.out.println("The number in not positive");
+        }*/
+
+        //ÜLESANNE 16
+/*        Scanner input = new Scanner(System.in);
+        System.out.print("Type a number: ");
+        int oddNumber = Integer.parseInt(input.nextLine());
+        if (oddNumber % 2 == 0) {
+            System.out.println("Number " + oddNumber + " is even ");
+        } else {
+            System.out.println("Number " + oddNumber + " is odd ");
+        }*/
+
+
+        // ÜLESANNE 18
+        /*Scanner input = new Scanner(System.in);
+        System.out.print("Type the points [0-60]: ");
+        int grade = Integer.parseInt(input.nextLine());
+        if (grade <= 29) {
+            System.out.println("failed");
+        }
+        else if (grade >= 30 && grade <= 34 )
+    {
+     System.out.println("Grade: 1");
+        }
+        else if (grade >= 35 && grade <= 39 ) {
+            System.out.println("Grade: 2");
+        }
+        else if (grade >= 40 && grade <= 44 ) {
+            System.out.println("Grade: 3");
+        }
+        else if (grade >= 45 && grade <= 49 ) {
+            System.out.println("Grade: 4");
+        }
+        else  {
+            System.out.println("Grade: 5");
+        }
+*/
+        //ÜLESANNE 19
+/*        Scanner input = new Scanner(System.in);
+        System.out.print("How old are you? ");
+        int age = Integer.parseInt(input.nextLine());
+        if (age >= 0 && age <=120) {
+            System.out.println("OK");
+        } else {
+            System.out.println("Impossible!");
+        }*/
+
+
+
+    }
+}
